44e31db6d2c2497f09d947b5a17cb9d8208b1ba9
Suraj <hrishabhsuraj52@gmail.com>
2018-12-07 15:00:26 +0530

    Update fetch_url.py

--- fetch_url.py
+++ fetch_url.py
@@ -1,31 +1,50 @@
+#
+# Complete the 'crawl' function below.
+#Reading Input
+import sys
 import re
 import time
-def get_hyperlinks(url):
-    web = {'https://www.wolfram.com': ['https://community.wolfram.com', 'https://www.wolfram.com/contact-us', 'https://www.wolfram.com', 'https://www.wolfram.com/about', 'https://www.wolfram.com/education'], \
-           'https://www.wolfram.com/education': ['/pro', '/web-apps', '/pro/pricing', '/pro/pricing'], \
-           'https://www.wolfram.com/about': ['https://community.wolfram.com', 'https://www.wolfram.com/contact-us', 'https://www.wolfram.com', 'https://www.wolfram.com/about', 'https://www.wolfram.com/education'], \
-           'https://www.wolfram.com/pro': ['/web-apps', '/pro/pricing', '/pro/pricing'], \
-           'https://www.wolfram.com/examples/pro-features/image-input/index.html': ['/examples/mathematics/index.html', '/examples/society-and-culture/points-of-interest/index.html', '/examples/science-and-technology/materials/index.html', '/examples/science-and-technology/physics/index.html'], \
-           'https://www.wolfram.com/examples/science-and-technology/engineering/index.html': ['/examples/mathematics/index.html', '/examples/society-and-culture/points-of-interest/index.html', '/input', '/examples/science-and-technology/materials/index.html']}
-    regex = re.compile(
-        r'^(?:http|ftp)s?://' # http:// or https://
-        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
-        r'localhost|' #localhost...
-        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
-        r'(?::\d+)?' # optional port
-        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
-    assert re.match(regex, url) is not None, "%s is invalid url. Failing." % url
-    time.sleep(0.02)
-    return web.get(url, [])
+import threading
+Input=[]
+while True:
+    input_ =input()
+    if input_ == 'EOF':
+        break
+    Input.append(input_)
+#print(Input)       
+# The function is expected to return a LIST
+# The function accepts following parameters:
+#  1. STRING root_url
+#  2. INTEGER n_workers
+#  3. FUNCTION get_hyperlinks
 
-url = 'https://www.wolfram.com'
-
-def ParseUrl(Url):
-    FetchedTags =[]
-    for i in get_hyperlinks(Url):
-        FetchedTags.append(i)
-    return sorted(FetchedTags)
+root_url = str(Input[0])
+n_workers = int(Input[1])
+get_hyperlinks = Input[2:]
+get_hyperlinks="\n".join(get_hyperlinks)
+exec(get_hyperlinks)
+global Sections
+Sections = get_hyperlinks(root_url)
 
 
+# print(get_hyperlinks)
 
-print(ParseUrl(url))
+
+
+def crawl(root_url, n_workers, get_hyperlinks):
+    OutputTags=[]
+    for i in Sections:
+        OutputTags.append(i)
+    
+        
+#    exec(get_hyperlinks)
+#    print(get_hyperlinks(root_url))
+
+#    OutputTags=[]
+#    for i in get_hyperlinks(root_url):
+#        OutputTags.append(i)
+#    return get_hyperlinks(root_url)
+
+print(crawl(root_url,n_workers, get_hyperlinks))
+
+    # Write your code here
