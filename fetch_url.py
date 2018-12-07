c40a562a5865950c791dd01bf237f9830f92348b
Suraj <hrishabhsuraj52@gmail.com>
2018-12-07 15:48:28 +0530

    Update fetch_url.py

--- fetch_url.py
+++ fetch_url.py
@@ -4,38 +4,41 @@
 import sys
 import re
 import time
-import threading
+from threading import Thread
 Input=[]
 while True:
-    input_ =input()
-    if input_ == 'EOF':
-        break
-    Input.append(input_)
+    try:
+        input_ =input().split()
+        if input_ == 'EOF':
+            break
+        Input.append(input_)
+    except EOFError:
+        pass
 #print(Input)       
 # The function is expected to return a LIST
 # The function accepts following parameters:
 #  1. STRING root_url
 #  2. INTEGER n_workers
 #  3. FUNCTION get_hyperlinks
-
+global get_hyperlinks,Sections
 root_url = str(Input[0])
 n_workers = int(Input[1])
 get_hyperlinks = Input[2:]
 get_hyperlinks="\n".join(get_hyperlinks)
 exec(get_hyperlinks)
-global Sections
-Sections = get_hyperlinks(root_url)
-
-
+def UniqueSection(url):
+    Sections = get_hyperlinks(url)
+    OutputTags=[]
+    for i in Sections:
+        if i.startswith('https')!=True:
+            OutputTags.append(root_url + i)
+    return OutputTags
 # print(get_hyperlinks)
 
 
-
-def crawl(root_url, n_workers, get_hyperlinks):
-    OutputTags=[]
-    for i in Sections:
-        OutputTags.append(i)
-    
+#def crawl(root_url, n_workers, get_hyperlinks):
+#    for j in range(n_workers)
+        
         
 #    exec(get_hyperlinks)
 #    print(get_hyperlinks(root_url))
@@ -45,6 +48,6 @@ def crawl(root_url, n_workers, get_hyperlinks):
 #        OutputTags.append(i)
 #    return get_hyperlinks(root_url)
 
-print(crawl(root_url,n_workers, get_hyperlinks))
+#print(crawl(root_url,n_workers, get_hyperlinks))
 
     # Write your code here
